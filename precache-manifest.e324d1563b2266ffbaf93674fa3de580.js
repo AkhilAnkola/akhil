9353cb6ead8b4159f4ebf5e6be92052077a027b4
akhil-ankola <akhil-ankola@github.com>
2020-08-14 00:34:22 +0530

    Updates

---
+++ precache-manifest.e324d1563b2266ffbaf93674fa3de580.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed04cf969a4950a6470b6bf759d7ddb6",
+    "url": "/akhil/index.html"
+  },
+  {
+    "revision": "4895f44d96a9d11b3266",
+    "url": "/akhil/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "f580efe82d1a911982dd",
+    "url": "/akhil/static/css/main.797c1bde.chunk.css"
+  },
+  {
+    "revision": "4895f44d96a9d11b3266",
+    "url": "/akhil/static/js/2.a705376d.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/akhil/static/js/2.a705376d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f580efe82d1a911982dd",
+    "url": "/akhil/static/js/main.99e2516c.chunk.js"
+  },
+  {
+    "revision": "c6583a95792f9e5c5896",
+    "url": "/akhil/static/js/runtime-main.cd34350f.js"
+  },
+  {
+    "revision": "9a013de96e0874ab4bceb2059578f213",
+    "url": "/akhil/static/media/about.9a013de9.png"
+  },
+  {
+    "revision": "4edfe8c9b9b75edf49f79064ed8c4c13",
+    "url": "/akhil/static/media/app_development.4edfe8c9.png"
+  },
+  {
+    "revision": "39d81dc728e096a6807f4e65cf08a342",
+    "url": "/akhil/static/media/awebz.39d81dc7.jpg"
+  },
+  {
+    "revision": "11a0297c7495aef9e5c167650f42cbff",
+    "url": "/akhil/static/media/books.11a0297c.jpg"
+  },
+  {
+    "revision": "01ef3362f7e7f55bd5f855827c48cc8c",
+    "url": "/akhil/static/media/calander.01ef3362.JPG"
+  },
+  {
+    "revision": "0a75991cd9df1d85e7345e7dc86a9a06",
+    "url": "/akhil/static/media/certificate.0a75991c.jpg"
+  },
+  {
+    "revision": "135f67036379bdbbd0576aedbe40886d",
+    "url": "/akhil/static/media/cursor.135f6703.JPG"
+  },
+  {
+    "revision": "ba354d9b027b0a070c47c294450b2575",
+    "url": "/akhil/static/media/facebook.ba354d9b.svg"
+  },
+  {
+    "revision": "a92c29e6ba02b880a86160686d8ff6f2",
+    "url": "/akhil/static/media/front_end_development.a92c29e6.png"
+  },
+  {
+    "revision": "001c141dd3d30ca1f6cd98a5a30654c8",
+    "url": "/akhil/static/media/galaxy.001c141d.JPG"
+  },
+  {
+    "revision": "72191166905f720e0437137dc1336654",
+    "url": "/akhil/static/media/game.72191166.JPG"
+  },
+  {
+    "revision": "eecf331151f0c5dd5c11c9a6bbeac7dd",
+    "url": "/akhil/static/media/graphics_designing.eecf3311.jpg"
+  },
+  {
+    "revision": "9e7ba2ffe463cc663381db8973efefb8",
+    "url": "/akhil/static/media/hidden_color.9e7ba2ff.JPG"
+  },
+  {
+    "revision": "7d3d540295b556da4b4c8cf6e373c04b",
+    "url": "/akhil/static/media/homeimg.7d3d5402.svg"
+  },
+  {
+    "revision": "73daa35d4dd3511daf86ff1b59575118",
+    "url": "/akhil/static/media/linkedin.73daa35d.svg"
+  },
+  {
+    "revision": "3d3aacc48f9638cae0f2d1bac12db1a1",
+    "url": "/akhil/static/media/paint.3d3aacc4.JPG"
+  },
+  {
+    "revision": "a31c54efe5c719a166a0856ad60fca0a",
+    "url": "/akhil/static/media/sketched.a31c54ef.svg"
+  },
+  {
+    "revision": "5c0499deb1484dd1ca2821affaebc849",
+    "url": "/akhil/static/media/software_development.5c0499de.png"
+  },
+  {
+    "revision": "53322d55a7b4dc9bd6092e551415f74e",
+    "url": "/akhil/static/media/table.53322d55.JPG"
+  },
+  {
+    "revision": "ba9890019b99c57397e3ee66d3041515",
+    "url": "/akhil/static/media/ui_development.ba989001.png"
+  },
+  {
+    "revision": "a042a26e566fba537ada0d60a29727ed",
+    "url": "/akhil/static/media/ux.a042a26e.jpg"
+  },
+  {
+    "revision": "fcde8f894355c7a6e8c1c639250e5d5f",
+    "url": "/akhil/static/media/waterdrop.fcde8f89.jpg"
+  },
+  {
+    "revision": "46945dce207a1828e3eb05157ef81cea",
+    "url": "/akhil/static/media/web_designing.46945dce.jpg"
+  },
+  {
+    "revision": "fe7c0311221c1a3a3532c3670c3ef218",
+    "url": "/akhil/static/media/web_development.fe7c0311.jpg"
+  },
+  {
+    "revision": "fb6d31e9498b7d08e224f5ba171f9214",
+    "url": "/akhil/static/media/wordpress.fb6d31e9.jpg"
+  }
+]);
